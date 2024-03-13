cc964b6594283a10a5c4920030d3f16897199acf
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-03-13 14:59:13 +0500

    ASSIGNMENT # 3 Half complete

---
+++ app.js
@@ -0,0 +1,34 @@
+//Chapter No: 17-20
+
+//Q1
+// var arr = [[],[]];
+// console.log(arr);
+
+//Q2
+// var arr = [[0, 1, 2, 3],[1, 0, 1, 2],[2, 1, 0, 1]];
+// console.log(arr);
+
+//Q3
+// for(var i = 1; i <= 10; i++) {
+//     document.write(i, "</br>")
+// }
+
+//Q4
+// var tableNum = parseInt(prompt("Enter a number to show its multiplication table"));
+// var tableLength = parseInt(prompt("Enter table length"));
+// document.write(`<b>Multiplication table of ${tableNum} </br> Length ${tableLength}</b> </br> </br>`);
+
+// for(var i = 1; i <= tableLength; i++) {
+//     document.write(`${tableNum} x ${i} = ${tableNum * i} </br>`);
+// }
+
+//Q5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for(var i = 0; i <= fruits.length; i++) {
+//     document.write(`${fruits[i]} </br>`);
+// }
+
+// document.write(`</br> </br> Element at index 0 ${fruits[0]} </br> Element at index 1 ${fruits[1]} </br> Element at index 2 ${fruits[2]} </br> Element at index 3 ${fruits[3]} </br> Element at index 4 ${fruits[4]} </br>`);
+
+//Q6
