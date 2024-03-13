a132afc39c8fa94dc8d8bc6bbe36c26d8bf1ea9e
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-03-13 16:17:05 +0500

    ASSIGNMENT # 5 First Part complete

--- app.js
+++ app.js
@@ -32,3 +32,85 @@
 // document.write(`</br> </br> Element at index 0 ${fruits[0]} </br> Element at index 1 ${fruits[1]} </br> Element at index 2 ${fruits[2]} </br> Element at index 3 ${fruits[3]} </br> Element at index 4 ${fruits[4]} </br>`);
 
 //Q6
+// var count = [];
+// document.write(`<b>Counting:</b> </br> </br>`);
+
+// for(var i = 1; i <= 15; i++) {
+//     count.push(i);
+// }
+// document.write(count);
+
+// var revCount = [];
+// document.write(`</br> </br> <b>Reverse counting:</b> </br> </br>`);
+
+// for(var i = 10; i >= 1; i--) {
+//     revCount.push(i);
+// }
+// document.write(revCount);
+
+// var even = [];
+// document.write(`</br> </br> <b>Even:</b> </br> </br>`);
+
+// for(var i = 0; i <= 20; i += 2) {
+//     even.push(i);
+// }
+// document.write(even);
+
+// var odd = [];
+// document.write(`</br> </br> <b>Odd:</b> </br> </br>`);
+
+// for(var i = 1; i <= 19; i += 2) {
+//     odd.push(i);
+// }
+// document.write(odd);
+
+// var series = [];
+// document.write(`</br> </br> <b>Series:</b> </br> </br>`);
+
+// for(var i = 1; i <= 10; i++) {
+//     series.push(2 * i + "k ");
+// }
+// document.write(series);
+
+//Q7
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var userInput = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
+// var lowercas = userInput.toLowerCase();
+
+// for(var i = 0; i <= A.length; i++) {
+//     if(lowercas == A[i]) {
+//         document.write(`${lowercas} is <b>available</b> at index ${i} in our bakery`);
+//         break;
+//     } else {
+//         document.write(`We are sorry. ${lowercas} is <b>not available</b> in our bakery`);
+//         break;
+//     }
+// }
+
+//Q8
+// var A = [24, 53, 78, 91, 12];
+// var largest = 0;
+
+// for (let i = 0; i < A.length; i++) {
+//     if (A[i] > largest) {
+//         largest = A[i];
+//     }
+// }
+// document.write(`Array items: ${A} </br> The largest number is ${largest}`);
+
+//Q9
+// var A = [24, 53, 78, 91, 12];
+// var smallest = A[0];
+
+// for (let i = 0; i < A.length; i++) {
+//     if (A[i] < smallest) {
+//         smallest = A[i];
+//     }
+// }
+// document.write(`Array items: ${A} </br> The smallest number is ${smallest}`);
+
+//Q10
+// var table = 5;
+// for (var i = 1; i <= 20; i++) {
+//     document.write(table * i,", ");
+// }
